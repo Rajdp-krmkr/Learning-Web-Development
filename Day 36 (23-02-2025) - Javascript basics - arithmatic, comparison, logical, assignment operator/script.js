d7ce6ec78926e53b752ed06380e278dd3060960b
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2025-02-26 18:24:29 +0530

    Add Day 36: Include JavaScript basics examples for arithmetic, comparison, logical, and assignment operators with script integration

---
+++ Day 36 (23-02-2025) - Javascript basics - arithmatic, comparison, logical, assignment operator/script.js
@@ -0,0 +1,44 @@
+// let a = 9;
+// let b = 3;
+
+// console.log(a+b);
+// console.log(a - b);
+// console.log(a * b); // multiplication
+// console.log(a / b); // division
+
+// console.log(a % b); // remainder
+// // 9 % 3 = 0
+// console.log(a ** b); //exponentiation
+
+// let c = true;   // boolean true
+// let d = 1;      // true but represting as 1
+
+// console.log(c == d);
+// console.log(c === d);
+
+// let e = 5;
+// let f = "5";
+
+// console.log(e == f); // true
+// console.log(e === f); // false
+
+// let a = 5;
+// let b = "5";
+
+// console.log(a != b); // 5 != "5" false
+// console.log(a !== b); // 5 !== "5" true
+
+
+// console.log(5 > 3);  // true
+// console.log(5 > 8); // false
+// console.log(10 >= 5);   // true
+// console.log(10 <= 10);  // true
+
+let a = true;
+let b = false;
+
+console.log(a && b); // false
+console.log(a || b); // true
+
+console.log(!b);  // true
+
