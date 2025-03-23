9fd82aec2981b0ebd76fe5f1fee25985fab0b059
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2025-03-23 20:52:57 +0530

    Add Day 45: Create HTML structure and JavaScript examples for functions, including break and continue statements

---
+++ Day 45 (23-03-2025) - Functions/Part 1 - break and continue statment/script.js
@@ -0,0 +1,27 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   if (i == 5) {
+//     console.log("Hello World");
+//     break;
+//   }
+// }
+// 1
+// 2
+// 3
+// 4
+// 5
+// Hello World
+
+for (let i = 1; i <= 10; i++) {
+  //   console.log("Hello World");
+  if (i == 5) {
+    continue;
+  }
+  console.log(i);
+  //
+  //
+  //
+
+  //
+  //
+}
---
+++ Day 45 (23-03-2025) - Functions/Part 2 - Functions/script.js
@@ -0,0 +1,36 @@
+// f(x) = x^2 + 3x + 5;
+
+// variable = parameter;
+// f(2) = 2^2 + 3*2 + 5;
+
+function printer(paper, data) {
+  let ink = "black&white";
+  let Printed_paper = ink + paper + data;
+  console.log(Printed_paper);
+  return Printed_paper;
+}
+
+//home
+printer("A4", "Hello World");
+
+//office
+printer("A3", "User data");
+
+function add(a, b) {
+  return a + b + 10;
+}
+
+const sum = add(2, 3);
+
+console.log(sum);
+
+function allOut() {
+  let electricity = "220v";
+  let chemical = "ABC";
+  //code chemical evaporation
+}
+
+//function syntax:
+// 1. function declaration
+// 2. function parameter
+// 3. function return
